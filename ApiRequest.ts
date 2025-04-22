6720eeee73c35a04e70ddc5aa4794c5504eec4ca
DGryaznykh <dgryaznyh@adxnet.com>
2025-04-22 09:42:29 +0300

    init

---
+++ ApiRequest.ts
@@ -0,0 +1,30 @@
+import {
+  TRequestConfig,
+  TRequestParams
+} from './ApiRequest.d';
+import {
+  TRequestMethod
+} from './constants';
+import RequestAdapterFactory from './RequestAdapter/RequestAdapterFactory';
+import ApiResponse from './ApiResponse';
+
+export default class ApiRequest {
+  private _token: string | undefined
+
+  request<T = any>(
+    method: TRequestMethod,
+    url: string,
+    params?: TRequestParams,
+    config?: TRequestConfig
+  ): Promise<ApiResponse<T>> {
+    const adapter = RequestAdapterFactory.createRequestAdapter(method, config)
+    if (this._token) {
+      adapter.setToken(this._token)
+    }
+    return adapter.request(method, url, params, config)
+  }
+
+  setToken(token: string | undefined) {
+    this._token = token
+  }
+}
---
+++ predicates.ts
@@ -0,0 +1,14 @@
+import {
+  NoBodyMethods,
+  RequestMethods,
+  TNoBodyRequestMethod,
+  TRequestMethod
+} from './constants';
+
+export const isNoBodyRequestMethod = (method: TRequestMethod): method is TNoBodyRequestMethod => {
+  return NoBodyMethods.has(method)
+}
+
+export const isRequestMethod = (method: string): method is TRequestMethod => {
+  return (RequestMethods as Set<string>).has(method)
+}
